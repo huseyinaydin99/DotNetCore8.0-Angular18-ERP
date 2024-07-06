2028dce950f2ac28f686d45993ae624a39681894
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-07-06 17:20:18 +0300

    Layout tasarlandı ve sayfalar arası routing/rotalama yapıldı. AdminLTE teması giydirildi. Generic Blank ve Section Component tasarlandı. Navbar ve Mainsidebar düzenlendi.

--- ERPClient/src/app/components/home/home.component.ts
+++ ERPClient/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../modules/shared.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
