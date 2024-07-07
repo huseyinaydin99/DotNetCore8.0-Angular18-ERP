b62049867e846a08db47b71ee0dfb58e331c9728
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-07-07 20:58:36 +0300

    Sweetalert2/uyarı kütüphanesi NPM paketi kuruldu. SwalService/mesaj servisi yazıldı. Call Swal ve Call Toast tamamlandı.

--- ERPClient/src/app/components/home/home.component.ts
+++ ERPClient/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
+import { SwalService } from '../../services/swal.service';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,9 @@ import { SharedModule } from '../../modules/shared.module';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(private swal: SwalService){
+    this.swal.callSwal("Kaydı Sil","Kayıt silinsin mi?", ()=> {
+      alert('Silme işlemi başarılı');
+    });
+  }
 }
