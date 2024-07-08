1fadfe7d3bd99bd27e2724722240f923bb32c0de
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-07-08 18:33:39 +0300

    Müşteriler için CustomersController/WebAPI DELETE Delete metodu yazıldı. Mediator ile ilgili handler'lara ulaşıyor. CQRS ve Result Pattern ile müşteri bilgileri için Request/RequestHandler yazıldı. DeleteCustomers için.

--- ERPServer/ERPServer.Application/obj/Debug/net8.0/ERPServer.Application.AssemblyInfo.cs
+++ ERPServer/ERPServer.Application/obj/Debug/net8.0/ERPServer.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPServer.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+747b8ca56d00b4379f73cb447d191a74eab96608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48373fa39b0468c832f351777ac2810307391ef4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPServer.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPServer.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
