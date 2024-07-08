289d339995cb8e77c87e4ecec3e3cefceedf2d2a
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-07-08 17:14:07 +0300

    Customer Entity oluşturuldu. Customer Entity için Repository ve Configuration yazıldı. Migration edildi.

--- ERPServer/ERPServer.Infrastructure/Context/ApplicationDbContext.cs
+++ ERPServer/ERPServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ internal sealed class ApplicationDbContext : IdentityDbContext<AppUser, Identity
     {
     }
 
+    public DbSet<Customer> Customers { get; set; }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfigurationsFromAssembly(typeof(DependencyInjection).Assembly);
