0cd7255ff04382b532cfff8c7fcf293b0ca9ec4a
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-07-08 14:12:54 +0300

    Login/giriş sayfası tasarlandı. Login işlemi signIn fonksiyonu ile HttpService ile sağlandı. ErrorService ile hata işleme yapıldı. SwalService ile yapılan işlemler mesaj uyarısı çıkarıyor.

--- ERPClient/src/app/services/http.service.ts
+++ ERPClient/src/app/services/http.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { api } from '../constants';
 import { ResultModel } from '../models/result.model';
 import { AuthService } from './auth.service';
+import { ErrorService } from './error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,9 @@ export class HttpService {
 
   constructor(
     private http: HttpClient,
-    private auth: AuthService) {
+    private auth: AuthService,
+    private err: ErrorService
+  ) {
 
   }
   post<T>(apiUrl: string, body: any, callBack: (res: T) => void, errCallBack?: () => void){
@@ -25,6 +28,7 @@ export class HttpService {
         callBack(res.data);
       },
       error: (err: HttpErrorResponse) => {
+        this.err.errorHandler(err);
         if(errCallBack)
           errCallBack();
       }
